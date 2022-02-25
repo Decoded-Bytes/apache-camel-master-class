b76e5d4e4c7f540690739892158e54e02aee5504
Decoded Bytes <decodedbytes@605ive.com>
2022-02-25 12:00:12 -0500

    Lecture 8 Simple File Poller

---
+++ src/main/java/com/decodedbytes/routes/LegacyFileRoute.java
@@ -0,0 +1,15 @@
+package com.decodedbytes.routes;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LegacyFileRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+        from("file:src/data/input?fileName=inputFile.txt")
+                .routeId("inputFileId")
+                .to("file:src/data/output?fileName=outputFile.txt");
+    }
+}
