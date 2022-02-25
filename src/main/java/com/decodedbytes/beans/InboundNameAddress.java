a4a0cd64ecd546b8bcb5d1f50853fd51b962cc13
Decoded Bytes <decodedbytes@605ive.com>
2022-02-25 12:15:46 -0500

    Lecture 12 Bean IO Mapping
    convert CSV to POJO using Bean IO

---
+++ src/main/java/com/decodedbytes/beans/InboundNameAddress.java
@@ -0,0 +1,20 @@
+package com.decodedbytes.beans;
+
+import lombok.Data;
+
+@Data
+public class InboundNameAddress {
+
+    private String name;
+
+    private String houseNumber;
+
+    private String street;
+
+    private String city;
+
+    private String province;
+
+    private String postalCode;
+
+}
--- src/main/java/com/decodedbytes/routes/LegacyFileRoute.java
+++ src/main/java/com/decodedbytes/routes/LegacyFileRoute.java
@@ -1,6 +1,8 @@
 package com.decodedbytes.routes;
 
+import com.decodedbytes.beans.InboundNameAddress;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.dataformat.beanio.BeanIODataFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -8,19 +10,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class LegacyFileRoute extends RouteBuilder {
 
+    BeanIODataFormat inboundDataFormat = new BeanIODataFormat("InboundMessageBeanIOMapping.xml","inboundMessageStream");
     Logger logger = LoggerFactory.getLogger(getClass());
+
 
     @Override
     public void configure() throws Exception {
 
-        from("file:src/data/input?fileName=inputFile.csv")
+        from("file:src/data/input?fileName=inputFile.csv&noop=true")
                 .routeId("legacyFileRouteId")
                 .split(body().tokenize("\n",1,true))
+                .streaming()
+                .unmarshal(inboundDataFormat)
                 .process(exchange -> {
-                    String fileData = exchange.getIn().getBody(String.class);
-                    logger.info("Read File Data: " + fileData);
+                    InboundNameAddress inboundNameAddress = exchange.getIn().getBody(InboundNameAddress.class);
+                    logger.info(inboundNameAddress.toString());
+                    exchange.getIn().setBody(inboundNameAddress.toString());
                 })
-                .to("file:src/data/output?fileName=outputFile.csv&fileExist=append")
+                .to("file:src/data/output?fileName=outputFile.csv&fileExist=append&appendChars=\\n")
                 .end();
     }
 }
